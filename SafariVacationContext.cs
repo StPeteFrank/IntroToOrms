42268b12cbf092e787c27da1c25118bd1983f1e6
Frank Diaz <diazfrankp@gmail.com>
2018-12-17 12:26:11 -0500

    Initial commit. Set up SafariVacationContext.

---
+++ SafariVacationContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace IntroToOrms
+{
+  public partial class SafariVacationContext : DbContext
+  {
+    public SafariVacationContext()
+    {
+    }
+
+    public SafariVacationContext(DbContextOptions<SafariVacationContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseNpgsql("server=localhost;database=SafariVacation");
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+    }
+  }
+}
