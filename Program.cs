18e33f4e59cdb3090558c344fa97b5073f6380d8
Frank Diaz <diazfrankp@gmail.com>
2018-12-17 16:18:35 -0500

    Read and Udate statements completed.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace IntroToOrms
 {
@@ -28,9 +29,50 @@ namespace IntroToOrms
       // db.SaveChanges();
 
       //READ Data
+      //Display all animals
+      var allSeenAnimals = db.SeenAnimals;
+
+      foreach (var seenanimal in allSeenAnimals)
+      {
+        System.Console.WriteLine(seenanimal.Species);
+      }
+      //Display animals only seen in the jungle
+      var onlySeenInTheJungle = db.SeenAnimals.Where(seenanimal => seenanimal.LocationOfLastSeen == "Jungle");
+      System.Console.WriteLine("Animals seen in the Jungle");
+      foreach (var seenanimal in onlySeenInTheJungle)
+      {
+        System.Console.WriteLine(seenanimal.Species);
+      }
+
       //UPDATE Data
+      // Update the CountOfTimesSeen and LocationOfLastSeen for an animal
+      //Find the objects
+      var Tigers = db.SeenAnimals.FirstOrDefault(seenanimal => seenanimal.Id == 3);
+      if (Tigers != null)
+      {
+        //Update the data
+        Tigers.CountOfTimesSeen = 30;
+        Tigers.LocationOfLastSeen = "Dessert";
+
+        // Save the changes
+        db.SaveChanges();
+      }
+
+      var Lions = db.SeenAnimals.FirstOrDefault(seenanimal => seenanimal.Id == 1);
+      if (Lions != null)
+      {
+        Lions.LocationOfLastSeen = "Jungle";
+        db.SaveChanges();
+      }
+
+      var allanimals = db.SeenAnimals;
+      foreach (var animals in allanimals)
+      {
+        Console.WriteLine($"{animals.Id}, {animals.Species}");
+      }
       //DELETE Data
+
     }
   }
 }
-
+// var results = db.SeenAnimals.Select(seenanimal => new { seenanimal.CountOfTimesSeen, seenanimal.LocationOfLastSeen });
